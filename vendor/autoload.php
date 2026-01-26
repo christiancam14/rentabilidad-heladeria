7237a6b2b930986c02050298ebe6ea7dc6f9d8f8
Christian Camacho <chriscamacho1045@gmail.com>
2026-01-26 13:25:32 -0500

    Update file permissions for various vendor binaries to ensure executable access, add new dev file for Vite, and update composer.lock and manifest.json for dependency management improvements.

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -19,4 +19,4 @@ if (PHP_VERSION_ID < 50600) {
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit3c79e040a3570288a4004f205416801a::getLoader();
+return ComposerAutoloaderInita43e741fbd938f3b1ad1e5d0314cf9b9::getLoader();
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -4,7 +4,7 @@
 
 namespace Composer\Autoload;
 
-class ComposerStaticInit3c79e040a3570288a4004f205416801a
+class ComposerStaticInita43e741fbd938f3b1ad1e5d0314cf9b9
 {
     public static $files = array (
         '6e3fae29631ef280660b3cdad06f25a8' => __DIR__ . '/..' . '/symfony/deprecation-contracts/function.php',
@@ -8127,9 +8127,9 @@ class ComposerStaticInit3c79e040a3570288a4004f205416801a
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->prefixLengthsPsr4 = ComposerStaticInit3c79e040a3570288a4004f205416801a::$prefixLengthsPsr4;
-            $loader->prefixDirsPsr4 = ComposerStaticInit3c79e040a3570288a4004f205416801a::$prefixDirsPsr4;
-            $loader->classMap = ComposerStaticInit3c79e040a3570288a4004f205416801a::$classMap;
+            $loader->prefixLengthsPsr4 = ComposerStaticInita43e741fbd938f3b1ad1e5d0314cf9b9::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInita43e741fbd938f3b1ad1e5d0314cf9b9::$prefixDirsPsr4;
+            $loader->classMap = ComposerStaticInita43e741fbd938f3b1ad1e5d0314cf9b9::$classMap;
 
         }, null, ClassLoader::class);
     }
